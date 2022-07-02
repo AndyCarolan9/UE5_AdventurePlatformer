3f5d7f66bdb5110eb66c468d9b953353ae5f607e
Andrew Carolan <andycarolan9@gmail.com>
2022-07-02 17:03:14 +0100

    Created Base Collectable Class

---
+++ Source/AdventurePlatformer/CollectableActor.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CollectableActor.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+// Sets default values
+ACollectableActor::ACollectableActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Trigger Capsule"));
+	Capsule->SetCollisionProfileName(TEXT("Trigger"));
+	Capsule->SetupAttachment(RootComponent);
+
+	Capsule->OnComponentBeginOverlap.AddDynamic(this, &ACollectableActor::OnOverlapBegin);
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(Capsule);
+}
+
+// Called when the game starts or when spawned
+void ACollectableActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACollectableActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ACollectableActor::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor && (OtherActor != this))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Actor: %s"), *OtherActor->GetName());
+	}
+}
+
---
+++ Source/AdventurePlatformer/CollectableActor.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CollectableActor.generated.h"
+
+UCLASS()
+class ADVENTUREPLATFORMER_API ACollectableActor : public AActor
+{
+	GENERATED_BODY()
+	
+private:
+	UPROPERTY(EditAnywhere)
+	class UCapsuleComponent* Capsule;
+
+	UPROPERTY(EditAnywhere)
+	class UStaticMeshComponent* Mesh;
+
+public:	
+	// Sets default values for this actor's properties
+	ACollectableActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+};
