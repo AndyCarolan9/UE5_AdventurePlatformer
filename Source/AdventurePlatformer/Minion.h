4e2d5c320997ed263bc37cccf0106f7c57fbb203
Andrew Carolan <andycarolan9@gmail.com>
2022-10-01 17:24:43 +0100

    Create Minion Class. Created Blueprint of Class and Corresponding AI Objects

---
+++ Source/AdventurePlatformer/Minion.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseEnemy.h"
+#include "Minion.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ADVENTUREPLATFORMER_API AMinion : public ABaseEnemy
+{
+	GENERATED_BODY()
+	
+};
